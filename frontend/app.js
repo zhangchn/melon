f6077c76aacc334415bde782b6ddf9dd5b9447bc
Zhang Chen <tgfbeta@me.com>
2026-04-13 18:12:44 +0800

    Fix: Re-bind click handlers after goUp()
    After goUp() recreates the paths, click handlers need to be re-bound.
    Added setTimeout to re-bind after animation completes, same as drillDown().

--- frontend/app.js
+++ frontend/app.js
@@ -175,6 +175,8 @@ class App {
       if (isCenterClick) {
         // Clicking center goes up one level
         this.chart.goUp();
+        // Re-bind after goUp since paths are recreated
+        setTimeout(() => this._bindChartPaths(), this.chart.options.animationDuration + 50);
       } else if (d.data.is_dir && d.children && d.children.length > 0) {
         // Clicking a non-center directory drills down
         this.chart.drillDown(d);
